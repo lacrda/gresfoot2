ed56f462c77901d1f7f0b0cb2931b6d68b99da04
Guilherme Lacerda <lacerda@poli.ufrj.br>
2018-11-14 17:39:28 -0200

    action cards done

--- game.php
+++ game.php
@@ -13,6 +13,9 @@ if($_SESSION['com'] == ''){
 if($_SESSION['imp'] == ''){
 	$_SESSION['imp'] = 0;
 }
+
+echo($_SESSION['usuario']);
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -40,6 +43,10 @@ if($_SESSION['imp'] == ''){
 								$('#escolas').html(data);
 							}
 						});
+  		$("#test").click(function(){
+  			var user = $_SESSION['usuario'];
+  			alert(user);
+  		});		
   		});
     </script>
 
@@ -150,7 +157,7 @@ if($_SESSION['imp'] == ''){
                 </table>
           </div>
           <div class="col-md-6">
-            <div class="row actions hidden-xs" >
+            <!-- <div class="row actions hidden-xs" style="display: none">
               <div>
                 <table class="table table-bordered table-striped table-color table-condensed" style="font-size: 12px" >
                 <thead></thead>
@@ -197,37 +204,76 @@ if($_SESSION['imp'] == ''){
                 </tr>
                 </table>
               </div>   
-            </div>
-            <div class="row">
-                <div class="col-md-6"align="center">
-                    <div class="row prog-row">
+            </div> -->
+            
+      	
+                <div align="center">
+                	<div class="col-md-12">
+                    <div class="prog-row" >
                     <h4>Ritmo no Barracão</h4>
-                    <div class="progress" style="height: 25px">
-                    <div class=" progress-bar" style="width:<?= $_SESSION['bar'] ?>%; font-size: 16px;"><?= $_SESSION['bar'] ?></div>
+                    <div class="progress">
+                    <div class=" progress-bar " style="width:<?= $_SESSION['bar'] ?>%; font-size: 12px;"><?= $_SESSION['bar'] ?></div>
                     </div>
+                	</div>
                     </div>
-                    <div class="row prog-row">
-                    <h4>Moral com a Comunidade</h4>
-                     <div class="progress" style="height: 25px">
-                     <div class="progress-bar" style="width: <?= $_SESSION['com'] ?>%; font-size: 16px"><?= $_SESSION['com'] ?></div>
-                    </div>
-                    </div>
-                    <div class="row prog-row">
-                    <h4>Moral na Imprensa</h4>
-                     <div class="progress" style="height: 25px">
-                      <div class=" progress-bar"  style="width: <?= $_SESSION['imp'] ?>%; font-size: 16px"><?= $_SESSION['imp'] ?></div>
+                    <div class="col-md-12">
+                    <div class="prog-row" >
+                    <h4>Comunidade</h4>
+                     <div class="progress">
+                     <div class="progress-bar" style="width: <?= $_SESSION['com'] ?>%; font-size: 12px"><?= $_SESSION['com'] ?></div>
                     </div>
                     </div>
                 </div>
-                <div class="col-md-6" align="center">
-                    <h4>Ações Disponíveis</h4>
+                <div class="col-md-12">
+                    <div class="prog-row" >
+                    <h4>Desempenho nos Ensaios</h4>
+                     <div class="progress">
+                      <div class=" progress-bar"  style="width: <?= $_SESSION['des'] ?>%; font-size: 12px"><?= $_SESSION['des'] ?></div>
+                    </div>
+                </div>
+                    </div>
+                </div>
+  
+     
+            
+                <div class=" col-md-12" align="center" style="margin-top: 3px;margin-bottom: 3px;">
+                    <h4>Ações disponíveis para essa semana</h4>
                     <!--button text must have less than 45caracters-->
-                    <button type="button" class="btn btn-default btn-block btn-action" data-toggle="popover" data-placement="left" title="Custo R$ 50.000,00" data-content="Descrição Ação 1">Contratar Equipe Terceirizada </button>
-                    <button type="button" class="btn btn-default btn-block btn-action" data-toggle="popover" data-placement="left" title="Custo R$ 70.000,00" data-content="Descrição Ação 2">Abaixar Ingressos Feijoada</button>
-                    <button type="button" class="btn btn-default btn-block btn-action" data-toggle="popover" data-placement="left" title="Custo R$ 5.000,00" data-content="Descrição Ação 3">Convidar Imprensa para uma Coletiva</button>
-                    <button type="button" class="btn btn-default btn-block btn-action" data-toggle="popover" data-placement="left" title="Custo R$ 2.000,00" data-content="Descrição Ação 4">Divulgar Samba na rádio</button>
+                    <div class="col-md-4">
+                   	<div class="panel panel-default" >
+					<div class="panel-heading">
+					<h6 class="panel-title">Opção 1</h6>
+					 </div>
+					<div class="panel-body" style="box-sizing: border-box;height: 70px">
+					Contratar Equipe Terceirizada
+					</div>
+                    <button   id="test" type="button" class="btn btn-default btn-block btn-action" style="margin-bottom: 0px;">Selecionar</button>
+					</div> 
+                	</div>
+                	<div class="col-md-4">
+                   	<div class="panel panel-default">
+					<div class="panel-heading">
+					<h6 class="panel-title">Opção 2</h6>
+					 </div>
+					<div class="panel-body" style="box-sizing: border-box;height: 70px">
+					Fazer Feijoada na Quadra
+					</div>
+                    <button type="button" class="btn btn-default btn-block btn-action" style="margin-bottom: 0px">Selecionar</button>
+					</div> 
+                	</div>
+                	<div class="col-md-4">
+                   	<div class="panel panel-default">
+					<div class="panel-heading">
+					<h6 class="panel-title">Opção 3</h6>
+					 </div>
+					<div class="panel-body" style="box-sizing: border-box;height: 70px">
+					Divulgar Samba
+					</div>
+                    <button type="button" class="btn btn-default btn-block btn-action" style="margin-bottom: 0px">Selecionar</button>
+					</div> 
+                	</div>
+                	
                 </div>
-            </div>
           </div>
           <div class="col-md-3">
         	<!-- Nav tabs -->
@@ -260,22 +306,22 @@ if($_SESSION['imp'] == ''){
                           <td>Semana 2</td>
                           <td>Semana 3</td>
                           <td>Semana 4</td>
-                          <td>Semana 1</td>
-                          <td>Semana 2</td>
-                          <td>Semana 3</td>
-                          <td>Semana 4</td>
-                          <td>Semana 1</td>
-                          <td>Semana 2</td>
-                          <td>Semana 3</td>
-                          <td>Semana 4</td>
-                          <td>Semana 1</td>
-                          <td>Semana 2</td>
-                          <td>Semana 3</td>
-                          <td>Semana 4</td>
-                          <td>Semana 1</td>
-                          <td>Semana 2</td>
-                          <td>Semana 3</td>
-                          <td>Semana 4</td>
+                          <td>Semana 5</td>
+                          <td>Semana 6</td>
+                          <td>Semana 7</td>
+                          <td>Semana 8</td>
+                          <td>Semana 9</td>
+                          <td>Semana 10</td>
+                          <td>Semana 11</td>
+                          <td>Semana 12</td>
+                          <td>Semana 13</td>
+                          <td>Semana 14</td>
+                          <td>Semana 15</td>
+                          <td>Semana 16</td>
+                          <td>Semana 17</td>
+                          <td>Semana 18</td>
+                          <td>Semana 19</td>
+                          <td>Semana 20</td>
                         </tr>
                         <tr> 
                           <td style="font-weight: bold">Calendário</td>
@@ -289,7 +335,7 @@ if($_SESSION['imp'] == ''){
                           <td></td>
                           <td class="activity">1º Ensaio</td>
                           <td></td>
-                          <td class="activity" style="font-size: 10px">Imprensa nos Barracões</td>
+                          <td class="activity" style="font-size: 10px">Barracões</td>
                           <td class="activity">2º Ensaio</td>
                           <td></td>
                           <td class="activity">3º Ensaio</td>
