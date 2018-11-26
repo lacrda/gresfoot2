fc7d2775f3066672239089904b0321c6ec0ec362
Guilherme Lacerda <lacerda@poli.ufrj.br>
2018-11-26 14:41:07 -0200

    db class novo

--- db.class.php
+++ db.class.php
@@ -1,37 +1,42 @@
 <?php
 
-	echo "teste push heroku";
+	class db{
 
-		
-// //host
-// 		private $host = 'localhost';
-
-
-// 		//usuario
-// 		private $usuario = 'root';
-
-// // 		//senha
-// // 		private $senha = '';
+		//host
+		private $host = 'us-cdbr-iron-east-01.cleardb.net';
 
 
-// // 		//bd para executar
-// // 		private $database = 'gresfoot';
-// 		// public function Conecta_mysql(){
+		//usuario
+		private $usuario = 'b3b2eb57f4bfd7';
+
+		//senha
+		private $senha = '4d4a033d';
+
+
+		//bd para executar
+		private $database = 'heroku_723cd138ea77e37';
+
+		public function Conecta_mysql(){
+			
+
+			$con = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database);
+			//ajustar  o charset de comunicação entre app e bd
+			mysqli_set_charset($con,'utf8');
+
+
+			//verificar conexão
+			if(mysqli_connect_errno()){
+				echo 'erro ao conectar com bd mysql'.mysqli_connect_error();
+			}
+
+			return $con;
 
 
 
-// // 			$con = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database);
-// // 			//ajustar  o charset de comunicação entre app e bd
-// 			mysqli_set_charset($con,'utf8');
+		}
 
 
-
-
-
-	// 	}
-
-
-	// }
+	}
 
 
 
